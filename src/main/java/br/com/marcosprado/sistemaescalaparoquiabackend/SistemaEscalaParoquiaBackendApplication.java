2853aff061c75b56f2df7055c3ccfe6dc27f50c0
Marcos Prado <marcospradofelix@gmail.com>
2025-03-29 00:58:29 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/br/com/marcosprado/sistemaescalaparoquiabackend/SistemaEscalaParoquiaBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemaescalaparoquiabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEscalaParoquiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaEscalaParoquiaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/marcosprado/sistemaescalaparoquiabackend/SistemaEscalaParoquiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcosprado.sistemaescalaparoquiabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEscalaParoquiaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
